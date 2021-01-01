2c082bbb1e8db43d63d7a0cea2e04fa91799ea60
DONGJINIM <idj7183@naver.com>
2021-01-01 22:33:36 +0900

    no message

--- src/main/java/com/example/demo/config/auth/SecurityConfig.java
+++ src/main/java/com/example/demo/config/auth/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.config.auth;
 
+import com.example.demo.domain.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .authorizeRequests()
                     .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**", "/profile").permitAll()
-                    .antMatchers("/api/v1/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     .anyRequest().authenticated()
                 .and()
                     .logout()
