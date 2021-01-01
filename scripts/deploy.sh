40222ac38cec4d35145b7340f1733f1ad854f0ab
DONGJINIM <idj7183@naver.com>
2021-01-01 22:06:10 +0900

    no message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,6 +21,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Dspring.config.location=/home/ec2-user/app/step2/zip/src/main/resources/application.properties,/home/ec2-user/app/application-oauth.properties \
+nohup java -jar -Dspring.config.location=/home/ec2-user/app/step2/zip/src/main/resources/application.properties,\
+   /home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,/home/ec2-user/app/application-real.properties \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
